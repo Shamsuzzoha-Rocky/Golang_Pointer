4fbd625278a70941d4d1c1997fa36d4cac80247e
“Shamsuzzoha-Rocky” <rhasanru@gmail.com>
2021-06-21 23:11:34 +0600

    practice

--- pointer.go
+++ pointer.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func main(){
-var x int
-var y *int
-fmt.Println("x value is", x)
-fmt.Println("x memory address is", &x)
+func main() {
+	var x int
+	var y *int
+	fmt.Println("x value is", x)
+	fmt.Println("x memory address is", &x)
 
-fmt.Println("y value is", y)
-fmt.Println("y memory address is", &y)
+	fmt.Println("y value is", y)
+	fmt.Println("y memory address is", &y)
 
-x=10
-y=&x
-fmt.Println("x is", x)
-fmt.Println("y is", y)
+	x = 12
+	y = &x
+	fmt.Println("x is", x)
+	fmt.Println("y is", y)
 
-fmt.Println("y dererencing value is", *y)
+	fmt.Println("y dererencing value is", *y)
 
 }
